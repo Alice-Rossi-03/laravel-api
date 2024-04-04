1e438b5506bbbf8899d914b420dfec76ce1136a1
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-04-04 12:40:48 +0200

    feat (ProjectCon) /testApi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,6 +56,10 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('dashboard.technologies.index') }}">Technologies</a>
                         </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('testApi') }}">Api</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
